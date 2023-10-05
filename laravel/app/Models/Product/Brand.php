9c70627bc628399c5abd52fae5fd23243041a1f2
Artur Miłkowski <artur-milkowski@tlen.pl>
2023-10-05 10:10:26 +0200

    Add the brand model

---
+++ laravel/app/Models/Product/Brand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models\Product;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['slug', 'name'];
+
+    /**
+     * The "booted" method of the model.
+     */
+
+    /*
+    protected static function booted(): void
+    {
+        static::creating(function (Brand $brand) {            
+            $brand->slug = str()->slug($brand->name);
+        });
+        static::updating(function (Brand $brand) {            
+            $brand->slug = str()->slug($brand->name);
+        });
+    }
+    */
+    /*
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+    */
+}
